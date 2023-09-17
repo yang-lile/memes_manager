b375f5ccd3436cfee10bf7b9385844285dcdbb66
优雅开源实验室 <2922979526@qq.com>
2023-09-17 21:49:10 +0800

    ✨ feat(memes_package): 建立了表情包库模型和存储库

---
+++ lib/memes_package/repository/memes_package_repository.dart
@@ -0,0 +1,44 @@
+import 'dart:io';
+
+import 'package:memes_manager/memes_package/model/memes_package_model.dart';
+import 'package:memes_manager/work_directory/service/work_directory_service.dart';
+import 'package:path/path.dart' as p;
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'memes_package_repository.g.dart';
+
+@riverpod
+MemesPackageRepository memesPackageRepository(MemesPackageRepositoryRef ref) =>
+    MemesPackageRepository(ref.watch(workDirectoryServiceProvider));
+
+class MemesPackageRepository {
+  MemesPackageRepository(this._workDirectoryService);
+
+  final WorkDirectoryService _workDirectoryService;
+
+  Future<List<MemesPackageModel>> getAllMemesPackage() async {
+    final workDirectory = _workDirectoryService.getWorkDirecory();
+    final workDirectories = workDirectory.list();
+    final memesPackageModels = <MemesPackageModel>[];
+
+    await for (final element in workDirectories) {
+      if (element.statSync().type == FileSystemEntityType.directory) {
+        final directory = element as Directory;
+
+        final basenameWithoutExtension =
+            p.basenameWithoutExtension(directory.path);
+        final split = basenameWithoutExtension.split('_');
+        final count = await directory.list().length;
+
+        final memesPackageModel = MemesPackageModel(
+          packageName: split[0],
+          author: split[1],
+          count: count,
+        );
+
+        memesPackageModels.add(memesPackageModel);
+      }
+    }
+    return memesPackageModels;
+  }
+}
